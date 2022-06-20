48d2cbfdd351b707341c65fa740a950412499832
82105 <82105@LAPTOP-4NIR835A>
2022-06-20 21:13:50 +0900

    master branch change

--- HelloGit/src/MyHello.java
+++ HelloGit/src/MyHello.java
@@ -3,5 +3,6 @@ public class MyHello {
 	public static void main(String args[]) {
 		System.out.println("Hello Git!");
 		System.out.println("It's second file.");
+		System.out.println("SKU");
 	}
 }
